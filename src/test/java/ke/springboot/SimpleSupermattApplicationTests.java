b32f1fa05d44f7aa0197b3eedefbad93fc96e40a
deepeters <dennis95peters@gmail.com>
2022-03-19 16:08:48 +0300

    initial commit to add project files and folders for the application

---
+++ src/test/java/ke/springboot/SimpleSupermattApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSupermattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
