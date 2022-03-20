617411fa830790f7501fb6ef5fb9db2cb3c6f190
deepeters <dennis95peters@gmail.com>
2022-03-20 19:42:40 +0300

    linked my backend routes to my frontend UI

--- src/main/webapp/supermatt-frontend/src/components/Product.js
+++ src/main/webapp/supermatt-frontend/src/components/Product.js
@@ -1,26 +1,62 @@
 import React from "react";
 
-import { Card, Form, Col, Button } from "react-bootstrap";
+import { Card, Form, Col, Row, Button } from "react-bootstrap";
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", category: "", description: "" };
+  }
+
+  submitProduct(event) {
+    alert(this.state.name);
+    event.preventDefault();
+  }
+
+  productChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
   render() {
     return (
       <Card className="border border-dark bg-dark text-white">
         <Card.Header>Add a Product</Card.Header>
-        <Form>
+        <Form onSubmit={this.submitProduct} id="productFormId">
           <Card.Body>
             <Form>
-              <Form.Group as={Col}>
-                <Form.Label>Product Name</Form.Label>
-                <Form.Control type="text" class={"bg-dark text-white"} placeholder="Enter Product Name" />
-              </Form.Group>
+              <Row>
+                <Form.Group as={Col} controlId="formGridTitle">
+                  <Form.Label>Product Name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.productChange}
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Product Name"
+                  />
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label>Product Category</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="category"
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Product Category"
+                  />
+                </Form.Group>
+              </Row>
               <Form.Group as={Col}>
                 <Form.Label>Product Description</Form.Label>
-                <Form.Control type="text" class={"bg-dark text-white"} placeholder="Enter Product Description" />
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Label>Product Category</Form.Label>
-                <Form.Control type="text" class={"bg-dark text-white"} placeholder="Enter Product Category" />
+                <Form.Control
+                  type="text"
+                  name="description"
+                  className={"bg-dark text-white"}
+                  placeholder="Enter Product Description"
+                />
               </Form.Group>
             </Form>
           </Card.Body>
