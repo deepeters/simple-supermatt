fbfe19edad6580fa9f021ee70c08d1a10728f421
deepeters <dennis95peters@gmail.com>
2022-03-20 10:17:38 +0300

    Add form to accept new input and table to display existing data

--- src/main/webapp/supermatt-frontend/src/components/Product.js
+++ src/main/webapp/supermatt-frontend/src/components/Product.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+
+import { Card, Form, Col, Button } from "react-bootstrap";
 
 class Product extends React.Component {
-    render(){
-        return(
-            <div className="text-white">
-                Add Product
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Card className="border border-dark bg-dark text-white">
+        <Card.Header>Add a Product</Card.Header>
+        <Form>
+          <Card.Body>
+            <Form>
+              <Form.Group as={Col}>
+                <Form.Label>Product Name</Form.Label>
+                <Form.Control type="text" class={"bg-dark text-white"} placeholder="Enter Product Name" />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Product Description</Form.Label>
+                <Form.Control type="text" class={"bg-dark text-white"} placeholder="Enter Product Description" />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Product Category</Form.Label>
+                <Form.Control type="text" class={"bg-dark text-white"} placeholder="Enter Product Category" />
+              </Form.Group>
+            </Form>
+          </Card.Body>
+          <Card.Footer style={{ textAlign: "right" }}>
+            <Button size="sm" variant="success" type="submit">
+              Submit
+            </Button>
+          </Card.Footer>
+        </Form>
+      </Card>
+    );
+  }
 }
 
 export default Product;
--- src/main/webapp/supermatt-frontend/src/components/ProductList.js
+++ src/main/webapp/supermatt-frontend/src/components/ProductList.js
@@ -1,16 +1,32 @@
 import React from "react";
 
+import { Card, Table } from "react-bootstrap";
+
 class ProductList extends React.Component {
-
-    render(){
-        return(
-            <div className="text-white">
-                Product List
-            </div>
-        )
-    }
-
-
+  render() {
+    return (
+      <Card className="border border-dark bg-dark text-white">
+        <Card.Header>Product List</Card.Header>
+        <Card.Body>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Product Description</th>
+                <th>Product Category</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr align="center">
+                <td colSpan="6">No Books Available</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default ProductList;
