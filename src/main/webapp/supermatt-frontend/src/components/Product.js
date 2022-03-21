419daa5b7cb7008a6984d69295ccd32eb37c32e8
deepeters <dennis95peters@gmail.com>
2022-03-21 22:18:26 +0300

    improve UI by adding toast for notifications

--- src/main/webapp/supermatt-frontend/src/components/Product.js
+++ src/main/webapp/supermatt-frontend/src/components/Product.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import axios from "axios";
 
+import MyToast from "./MyToast";
+
 import { Card, Form, Col, Row, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faPlusSquare, faUndo } from "@fortawesome/free-solid-svg-icons";
@@ -10,6 +12,7 @@ class Product extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.initialState;
+    this.state.show = false;
     this.productChange = this.productChange.bind(this);
     this.submitProduct = this.submitProduct.bind(this);
   }
@@ -37,10 +40,14 @@ class Product extends React.Component {
 
     axios.post("http://localhost:8080/products", product).then((response) => {
       if (response.data != null) {
-        this.setState(this.initialState);
-        alert("Product Added Successfully");
+        this.setState({ show: true });
+        setTimeout(() => this.setState({ show: false }), 3000)
+      } else {
+        this.setState({ show: false });
+        
       }
     });
+    this.setState(this.initialState);
   };
 
   productChange = (event) => {
@@ -53,82 +60,87 @@ class Product extends React.Component {
     const { id, name, category, description } = this.state;
 
     return (
-      <Card className="border border-dark bg-dark text-white">
-        <Card.Header>
-          <FontAwesomeIcon icon={faPlusSquare} />
-          Add a Product
-        </Card.Header>
-        <Form onReset={this.resetProduct} onSubmit={this.submitProduct} id="productFormId">
-          <Card.Body>
-            <Form>
-              <Row>
-                <Form.Group as={Col} controlId="formGridId">
-                  <Form.Label>Product #</Form.Label>
+      <div>
+        <div style={{ display: this.state.show ? "block" : "none" }}>
+          <MyToast children={{ show: this.state.show, message: "Product Added Successfully." }} />
+        </div>
+        <Card className="border border-dark bg-dark text-white">
+          <Card.Header>
+            <FontAwesomeIcon icon={faPlusSquare} />
+            Add a Product
+          </Card.Header>
+          <Form onReset={this.resetProduct} onSubmit={this.submitProduct} id="productFormId">
+            <Card.Body>
+              <Form>
+                <Row>
+                  <Form.Group as={Col} controlId="formGridId">
+                    <Form.Label>Product #</Form.Label>
+                    <Form.Control
+                      required
+                      autoComplete="off"
+                      type="text"
+                      name="id"
+                      value={id}
+                      onChange={this.productChange}
+                      className={"bg-dark text-white"}
+                      placeholder="Enter Product ID"
+                    />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>Product Name</Form.Label>
+                    <Form.Control
+                      required
+                      autoComplete="off"
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={this.productChange}
+                      className={"bg-dark text-white"}
+                      placeholder="Enter Product Name"
+                    />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridCategory">
+                    <Form.Label>Product Category</Form.Label>
+                    <Form.Control
+                      required
+                      autoComplete="off"
+                      type="text"
+                      name="category"
+                      value={category}
+                      onChange={this.productChange}
+                      className={"bg-dark text-white"}
+                      placeholder="Enter Product Category"
+                    />
+                  </Form.Group>
+                </Row>
+                <Form.Group as={Col} controlId="formGridDescription">
+                  <Form.Label>Product Description</Form.Label>
                   <Form.Control
                     required
                     autoComplete="off"
                     type="text"
-                    name="id"
-                    value={id}
+                    name="description"
+                    value={description}
                     onChange={this.productChange}
                     className={"bg-dark text-white"}
-                    placeholder="Enter Product ID"
+                    placeholder="Enter Product Description"
                   />
                 </Form.Group>
-                <Form.Group as={Col} controlId="formGridName">
-                  <Form.Label>Product Name</Form.Label>
-                  <Form.Control
-                    required
-                    autoComplete="off"
-                    type="text"
-                    name="name"
-                    value={name}
-                    onChange={this.productChange}
-                    className={"bg-dark text-white"}
-                    placeholder="Enter Product Name"
-                  />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridCategory">
-                  <Form.Label>Product Category</Form.Label>
-                  <Form.Control
-                    required
-                    autoComplete="off"
-                    type="text"
-                    name="category"
-                    value={category}
-                    onChange={this.productChange}
-                    className={"bg-dark text-white"}
-                    placeholder="Enter Product Category"
-                  />
-                </Form.Group>
-              </Row>
-              <Form.Group as={Col} controlId="formGridDescription">
-                <Form.Label>Product Description</Form.Label>
-                <Form.Control
-                  required
-                  autoComplete="off"
-                  type="text"
-                  name="description"
-                  value={description}
-                  onChange={this.productChange}
-                  className={"bg-dark text-white"}
-                  placeholder="Enter Product Description"
-                />
-              </Form.Group>
-            </Form>
-          </Card.Body>
-          <Card.Footer style={{ textAlign: "right" }}>
-            <Button size="sm" variant="success" type="submit">
-              <FontAwesomeIcon icon={faSave} />
-              Submit
-            </Button>{" "}
-            <Button size="sm" variant="info" type="reset">
-              <FontAwesomeIcon icon={faUndo} />
-              Reset
-            </Button>
-          </Card.Footer>
-        </Form>
-      </Card>
+              </Form>
+            </Card.Body>
+            <Card.Footer style={{ textAlign: "right" }}>
+              <Button size="sm" variant="success" type="submit">
+                <FontAwesomeIcon icon={faSave} />
+                Submit
+              </Button>{" "}
+              <Button size="sm" variant="info" type="reset">
+                <FontAwesomeIcon icon={faUndo} />
+                Reset
+              </Button>
+            </Card.Footer>
+          </Form>
+        </Card>
+      </div>
     );
   }
 }
