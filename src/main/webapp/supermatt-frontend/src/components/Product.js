4c38f98745578acab00215c89b3c74881564f1ad
deepeters <dennis95peters@gmail.com>
2022-03-21 21:39:46 +0300

    enable post request from the UI using Axios

--- src/main/webapp/supermatt-frontend/src/components/Product.js
+++ src/main/webapp/supermatt-frontend/src/components/Product.js
@@ -1,54 +1,88 @@
 import React from "react";
 
+import axios from "axios";
+
 import { Card, Form, Col, Row, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { faSave, faPlusSquare, faUndo } from "@fortawesome/free-solid-svg-icons";
 
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", category: "", description: "" };
+    this.state = this.initialState;
     this.productChange = this.productChange.bind(this);
     this.submitProduct = this.submitProduct.bind(this);
   }
 
-  submitProduct(event) {
-    alert(
-      "Name: " +
-        this.state.name +
-        ", Category: " +
-        this.state.category +
-        ", Description: " +
-        this.state.description +
-        ""
-    );
-    event.preventDefault();
-  }
+  initialState = {
+    id: "",
+    name: "",
+    category: "",
+    description: "",
+  };
 
-  productChange(event) {
+  resetProduct = () => {
+    this.setState(() => this.initialState);
+  };
+
+  submitProduct = (event) => {
+    event.preventDefault();
+
+    const product = {
+      name: this.state.name,
+      category: this.state.category,
+      id: this.state.id,
+      description: this.state.description,
+    };
+
+    axios.post("http://localhost:8080/products", product).then((response) => {
+      if (response.data != null) {
+        this.setState(this.initialState);
+        alert("Product Added Successfully");
+      }
+    });
+  };
+
+  productChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
     });
-  }
+  };
 
   render() {
+    const { id, name, category, description } = this.state;
+
     return (
       <Card className="border border-dark bg-dark text-white">
         <Card.Header>
           <FontAwesomeIcon icon={faPlusSquare} />
           Add a Product
         </Card.Header>
-        <Form onSubmit={this.submitProduct} id="productFormId">
+        <Form onReset={this.resetProduct} onSubmit={this.submitProduct} id="productFormId">
           <Card.Body>
             <Form>
               <Row>
+                <Form.Group as={Col} controlId="formGridId">
+                  <Form.Label>Product #</Form.Label>
+                  <Form.Control
+                    required
+                    autoComplete="off"
+                    type="text"
+                    name="id"
+                    value={id}
+                    onChange={this.productChange}
+                    className={"bg-dark text-white"}
+                    placeholder="Enter Product ID"
+                  />
+                </Form.Group>
                 <Form.Group as={Col} controlId="formGridName">
                   <Form.Label>Product Name</Form.Label>
                   <Form.Control
                     required
+                    autoComplete="off"
                     type="text"
                     name="name"
-                    value={this.state.name}
+                    value={name}
                     onChange={this.productChange}
                     className={"bg-dark text-white"}
                     placeholder="Enter Product Name"
@@ -58,9 +92,10 @@ class Product extends React.Component {
                   <Form.Label>Product Category</Form.Label>
                   <Form.Control
                     required
+                    autoComplete="off"
                     type="text"
                     name="category"
-                    value={this.state.category}
+                    value={category}
                     onChange={this.productChange}
                     className={"bg-dark text-white"}
                     placeholder="Enter Product Category"
@@ -71,9 +106,10 @@ class Product extends React.Component {
                 <Form.Label>Product Description</Form.Label>
                 <Form.Control
                   required
+                  autoComplete="off"
                   type="text"
                   name="description"
-                  value={this.state.description}
+                  value={description}
                   onChange={this.productChange}
                   className={"bg-dark text-white"}
                   placeholder="Enter Product Description"
@@ -85,6 +121,10 @@ class Product extends React.Component {
             <Button size="sm" variant="success" type="submit">
               <FontAwesomeIcon icon={faSave} />
               Submit
+            </Button>{" "}
+            <Button size="sm" variant="info" type="reset">
+              <FontAwesomeIcon icon={faUndo} />
+              Reset
             </Button>
           </Card.Footer>
         </Form>
